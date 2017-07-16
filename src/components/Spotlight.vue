ffe723fa40ddfd237e6fac82801438024e3fbf44
monkick <saruyama.monki@gmail.com>
2017-07-16 15:03:10 +0900

    color gradient

--- src/components/Spotlight.vue
+++ src/components/Spotlight.vue
@@ -1,5 +1,7 @@
 <template>
-    <div class="spotlight"></div>
+    <div class="spotlight-wrapper">
+        <div class="spotlight"></div>
+    </div>
 </template>
 <script>
     export default {
@@ -7,10 +9,14 @@
     }
 </script>
 <style lang="scss" scoped="true">
-    .spotlight {
-        width: 20vw;
-        height: 100vh;
-        background-color: #fff;
-        opacity: .5;
+    .spotlight-wrapper {
+        padding: 0 20px;
+
+        .spotlight {
+            width: 5vw;
+            height: 100vh;
+            background: linear-gradient(to bottom, rgba(255,255,255,1) 10%,rgba(255,255,255, .1) 100%);
+        }
     }
+
 </style>
