676643288b6987b4b2fa1e010a4b92bcb0296aec
monkick <saruyama.monki@gmail.com>
2017-07-17 13:03:35 +0900

    move spotight

--- src/components/Spotlight.vue
+++ src/components/Spotlight.vue
@@ -13,9 +13,28 @@
         padding: 0 20px;
 
         .spotlight {
+            position: relative;
+            top : -10px;
             width: 5vw;
             height: 100vh;
             background: linear-gradient(to bottom, rgba(255,255,255,1) 10%,rgba(255,255,255, .1) 100%);
+            transform-origin:center top;
+            transform: rotate(0deg);
+            animation-name: spotlight;
+            animation-duration: 1800ms;
+            animation-iteration-count: infinite;
+            animation-timing-function: ease-in-out;
+        }
+
+        @keyframes spotlight {
+            from, to {
+                transform: rotate(0deg);
+            }
+
+            35%,
+            50% {
+                transform: rotate(-20deg);
+            }
         }
     }
 
