38d2a150f1e5710f5490a799dc3c30f78e2a1d74
monkick <saruyama.monki@gmail.com>
2017-07-18 11:11:46 +0900

    spotight double

--- src/components/Spotlight.vue
+++ src/components/Spotlight.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="spotlight-wrapper">
         <div class="spotlight"></div>
+        <div class="spotlight"></div>
     </div>
 </template>
 <script>
@@ -10,20 +11,29 @@
 </script>
 <style lang="scss" scoped="true">
     .spotlight-wrapper {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        position: relative;
         padding: 0 20px;
 
         .spotlight {
             position: relative;
-            top : -10px;
+            top : -30px;
             width: 5vw;
-            height: 100vh;
+            height: 200vh;
             background: linear-gradient(to bottom, rgba(255,255,255,1) 10%,rgba(255,255,255, .1) 100%);
             transform-origin:center top;
             transform: rotate(0deg);
             animation-name: spotlight;
-            animation-duration: 1800ms;
+            animation-duration: 2000ms;
             animation-iteration-count: infinite;
             animation-timing-function: ease-in-out;
+        }
+
+        .spotlight:last-child {
+            right: 0;
+            animation-name: spotlight2;
         }
 
         @keyframes spotlight {
@@ -33,7 +43,18 @@
 
             35%,
             50% {
-                transform: rotate(-20deg);
+                transform: rotate(-45deg);
+            }
+        }
+
+        @keyframes spotlight2 {
+            from, to {
+                transform: rotate(0deg);
+            }
+
+            35%,
+            50% {
+                transform: rotate(45deg);
             }
         }
     }
